bb3798834e322934bea5160f5fa7c3050bc8e3ce
Charlie Kim <kimcharli@gmail.com>
2021-08-18 01:54:20 -0400

    add python3-venv

--- setup/setup-venv-ubuntu16.sh
+++ setup/setup-venv-ubuntu16.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo apt -y install python3.7 python3.7-distutils
+sudo apt -y install python3.7 python3.7-distutils python3-venv
 virtualenv -p python3.7 venv
 source venv/bin/activate
 pip install -r python/requirements.txt 
